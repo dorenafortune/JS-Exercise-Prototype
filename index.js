29facbc48e8eb395d676a39d11b9ea02d909e449
doren <56702391+doranium2013@users.noreply.github.com>
2019-11-13 15:08:10 -0800

    got first task done

--- index.js
+++ index.js
@@ -39,10 +39,23 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {    
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+};
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length < 10){ 
+    this.stomach.push(someFood);
+   }
+   else{return this.stomach}; //<---- does not need this, passes without it
+  };
+Person.prototype.poop = function(){
+  this.stomach = [];
 }
-
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+};
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -57,8 +70,8 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGalon) {
+  
 }
 
 /*
